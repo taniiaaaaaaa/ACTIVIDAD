b92130eaaaca8af8a74d88bf30f33efc231d7d24
taniiaaaaaaa <158506158+taniiaaaaaaa@users.noreply.github.com>
2024-04-03 13:32:39 -0600

    Update FUNNEL.py

--- FUNNEL.py
+++ FUNNEL.py
@@ -18,12 +18,12 @@ st.title("ACTIVIDAD")
 st.text('La presente página permite visualizar la actividad de los socios en el tiempo, tal como se puede ver en la siguiente tabla de valores.')
 st.markdown(f' Tabla de actividad de socios')
 df=pd.read_csv('ACTIVIDAD (1).csv',encoding='latin-1')
+df=df.drop('Unnamed: 0',axis=1)
 st.write(df)
 
 st.text('La cual puede ser representada por medio de la siguiente gráfica.')
 df=df.set_index('FECHAS')
 df=df.drop('ALTAS SOCIOS',axis=1)
 df=df.drop('BAJAS SOCIOS',axis=1)
-df=df.drop('Unnamed: 0',axis=1)
 st.line_chart(df)
 
